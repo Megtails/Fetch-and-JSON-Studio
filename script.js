e162a2c1596247fe35cc1d7b840baf56449a29b8
Meghan <stlmeghan@yahoo.com>
2022-07-05 15:07:22 -0500

    forgot a t in active

--- script.js
+++ script.js
@@ -11,13 +11,14 @@ window.addEventListener("load", function() {
                     <h3>${json[i].firstName} ${json[i].lastName}</h3>
                         <ul>
                             <li>Hours in space: ${json[i].hoursInSpace}</li>
-                            <li>Active: ${json[i].acive}</li>
+                            <li>Active: ${json[i].active}</li>
                             <li>Skills: ${json[i].skills}</li>
                         </ul>
                 </div>
                 <img class="avatar" src=${json[i].picture}>
                 </div>
              `;
+                if ()
             }
         })
     });
